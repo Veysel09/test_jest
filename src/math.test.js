cf459f78f98fa0a59b35023cf2416737615e4978
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-09 12:02:33 +0300

    calculateTest

--- src/math.test.js
+++ src/math.test.js
@@ -13,3 +13,27 @@ const {
 //   celsiusToFahrenheit,
 //   fibonacci,
 // } from "./math";
+
+describe("calculateTotal", () => {
+  //test caselerii olusturuyoruz
+  it("should exist", () => {
+    expect(calculateTotal).toBeDefined;
+  });
+
+  it("should return a number", () => {
+    expect(calculateTotal(100, 0.2)).toBe(120);
+  });
+
+  it("should return with default VAT", () => {
+    expect(calculateTotal(200)).toBe(236);
+  });
+});
+describe("celsiusToFahrenheit", () => {
+  ///
+});
+describe("fahrenheitToCelsius", () => {
+  //
+});
+describe("fibonacci", () => {
+  //
+});
