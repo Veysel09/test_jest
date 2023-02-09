958ed98073981abef5ec6080654c4b4322542b0c
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-09 11:41:30 +0300

    import

---
+++ src/math.test.js
@@ -0,0 +1,15 @@
+//! node module import formati
+const {
+  calculateTotal,
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+  fibonacci,
+} = require("./math");
+
+//! ES6 module formati
+// import {
+//   calculateTotal,
+//   fahrenheitToCelsius,
+//   celsiusToFahrenheit,
+//   fibonacci,
+// } from "./math";
